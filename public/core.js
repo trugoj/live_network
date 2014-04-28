d29733b0c3f2ae289b9823bdc0f5bfa43aa8e349
trugoj <martin@ganymed.org>
2014-04-28 12:10:51 +0200

    visualization half done

--- public/core.js
+++ public/core.js
@@ -46,21 +46,18 @@ myApp.directive('donutChart', function(){
 myApp.directive('cluster', function(){
 	function link(scope, element, attr){
 		
-	var radius = 960 / 2;
-	var data = scope.data;
-
 	var data = {
 		  "nodes": [
 		    {
-		      "name": "AS&T",
+		      "name": "root.AS&T",
 		      "id": "id_1"
 		    },
 		    {
-		      "name": "Casualty R&D",
+		      "name": "root.Casualty R&D",
 		      "id": "id_2"
 		    },
 		    {
-		      "name": "Cat Modelling",
+		      "name": "root.Cat Modelling",
 		      "id": "id_3"
 		    }
 		  ],
@@ -78,47 +75,119 @@ myApp.directive('cluster', function(){
 		  ]
 		}
 
-	var cluster = d3.layout.cluster()
-	    .size([360, radius - 120]);
+		var diameter = 500,
+		    radius = diameter / 2,
+		    innerRadius = radius - 60;
 
-	var diagonal = d3.svg.diagonal.radial()
-	    .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+		var cluster = d3.layout.cluster()
+		    .size([360, innerRadius])
+		    .sort(null)
+		    .value(function(d) { return d.size; });
 
-	var svg = d3.select(element[0]).append("svg")
-	    .attr("width", radius * 2)
-	    .attr("height", radius * 2)
-	  .append("g")
-	    .attr("transform", "translate(" + radius + "," + radius + ")");
+		var bundle = d3.layout.bundle();
 
-	var n2 = cluster.nodes( data.nodes );
-	
-	d3.json("flare3.json", function(error, root) {
-	  var nodes = cluster.nodes(root);
+		var line = d3.svg.line.radial()
+		    .interpolate("bundle")
+		    .tension(.85)
+		    .radius(function(d) { return d.y; })
+		    .angle(function(d) { return d.x / 180 * Math.PI; });
 
-	  var link = svg.selectAll("path.link")
-	      .data(cluster.links(nodes))
-	    .enter().append("path")
-	      .attr("class", "link")
-	      .attr("d", diagonal);
+		var svg = d3.select(element[0]).append("svg")
+		    .attr("width", diameter)
+		    .attr("height", diameter)
+		  .append("g")
+		    .attr("transform", "translate(" + radius + "," + radius + ")");
 
-	  var node = svg.selectAll("g.node")
-	      .data(nodes.filter(function(n) { return !n.children; }))
-	    .enter().append("g")
-	      .attr("class", "node")
-	      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
-
-	  node.append("circle")
-	      .attr("r", 4.5);
-
-	  node.append("text")
-	      .attr("dy", ".31em")
-	      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-	      .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
-	      .text(function(d) { return d.name; });
-	});
-
-	d3.select(self.frameElement).style("height", radius * 2 + "px");	
+		//var n2 = scope.data.nodes;
 		
+		// d3.json("readme-flare-imports-2.json", function(error, classes) {
+		  var nodes = cluster.nodes(packageHierarchy(data.nodes));
+		  var links = treatLinks(data.links, nodes);
+
+		  svg.selectAll(".link")
+		      .data(bundle(links))
+		    .enter().append("path")
+		      .attr("class", "link")
+		      .attr("d", line);
+
+		  svg.selectAll(".node")
+		      .data(nodes.filter(function(n) { return !n.children; }))
+		    .enter().append("g")
+		      .attr("class", "node")
+		      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+		    .append("text")
+		      .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
+		      .attr("dy", ".31em")
+		      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+		      .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
+		      .text(function(d) { return d.key; });
+		// });
+
+		d3.select(self.frameElement).style("height", diameter + "px");
+
+		// Lazily construct the package hierarchy from class names.
+		function packageHierarchy(classes) {
+		  var map = {};
+
+		  function find(name, data) {
+		    var node = map[name], i;
+		    if (!node) {
+		      node = map[name] = data || {name: name, children: []};
+		      if (name.length) {
+			node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+			node.parent.children.push(node);
+			node.key = name.substring(i + 1);
+		      }
+		    }
+		    return node;
+		  }
+
+		  classes.forEach(function(d) {
+		    find(d.name, d);
+		  });
+
+		  return map[""];
+		}
+
+		function treatLinks(links, nodes) {
+		  var imports = [];
+		  var map = {};
+
+		  nodes.forEach( function(d) {
+		    map[d.id] = d;
+		  });
+
+		  links.forEach( function(d) {
+		  	var e = {};
+			e.source = map[d.source];
+			e.target = map[d.target]
+			imports.push(e);
+		  });
+
+		  return imports;
+		}
+		
+		// Return a list of imports for the given array of nodes.
+		function packageImports(nodes) {
+		  var map = {},
+		      imports = [];
+
+		  // Compute a map from name to node.
+		  nodes.forEach(function(d) {
+		    map[d.name] = d;
+		  });
+
+		  // For each import, construct a link from the source to target node.
+		  nodes.forEach(function(d) {
+		    if (d.imports) d.imports.forEach(function(i) {
+		      imports.push({source: map[d.name], target: map[i]});
+		    });
+		  });
+
+		  return imports;
+		}
+
+
 	}
   return {
   	link: link,
