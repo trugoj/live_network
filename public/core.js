b42c6cd8ff90ca4918e4d1d0a1d75e4bfb04db29
trugoj <martin@ganymed.org>
2014-04-28 15:52:25 +0200

    working

--- public/core.js
+++ public/core.js
@@ -45,36 +45,6 @@ myApp.directive('donutChart', function(){
 
 myApp.directive('cluster', function(){
 	function link(scope, element, attr){
-		
-	var data = {
-		  "nodes": [
-		    {
-		      "name": "root.AS&T",
-		      "id": "id_1"
-		    },
-		    {
-		      "name": "root.Casualty R&D",
-		      "id": "id_2"
-		    },
-		    {
-		      "name": "root.Cat Modelling",
-		      "id": "id_3"
-		    }
-		  ],
-		  "links": [
-		    {
-		      "source": "id_1",
-		      "target": "id_2",
-		      "group": "first"
-		    },
-		    {
-		      "source": "id_1",
-		      "target": "id_3",
-		      "group": "second"
-		    }
-		  ]
-		}
-
 		var diameter = 500,
 		    radius = diameter / 2,
 		    innerRadius = radius - 60;
@@ -98,34 +68,101 @@ myApp.directive('cluster', function(){
 		  .append("g")
 		    .attr("transform", "translate(" + radius + "," + radius + ")");
 
-		//var n2 = scope.data.nodes;
-		
-		// d3.json("readme-flare-imports-2.json", function(error, classes) {
-		  var nodes = cluster.nodes(packageHierarchy(data.nodes));
-		  var links = treatLinks(data.links, nodes);
-
-		  svg.selectAll(".link")
-		      .data(bundle(links))
-		    .enter().append("path")
-		      .attr("class", "link")
-		      .attr("d", line);
-
-		  svg.selectAll(".node")
-		      .data(nodes.filter(function(n) { return !n.children; }))
-		    .enter().append("g")
-		      .attr("class", "node")
-		      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
-		    .append("text")
-		      .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
-		      .attr("dy", ".31em")
-		      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-		      .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
-		      .text(function(d) { return d.key; });
-		// });
-
 		d3.select(self.frameElement).style("height", diameter + "px");
 
-		// Lazily construct the package hierarchy from class names.
+        scope.$watch('data', function(dat) {
+            console.log("an element within `nodes` changed!");
+            console.log(dat);
+
+            update_graph(dat);
+        }, true);
+
+        function update_graph(dat) {
+            if (dat.nodes)
+                if (dat.edges) {
+                    nclone = clone(dat.nodes);
+                    lclone = clone(dat.edges);
+
+                    var nodes = cluster.nodes(packageHierarchy(nclone));
+                    var links = treatLinks(lclone, nodes);
+
+                    var l = svg.selectAll(".link")
+                        .data(bundle(links))
+
+                    //Update
+                    l.transition().attr("d", line);
+
+                    l.enter().append("path")
+                        .attr("class", "link")
+                        .attr("d", line);
+
+                    l.exit().remove();
+
+                    var n = svg.selectAll(".node")
+                        .data(nodes.filter(function (n) {
+                            return !n.children;
+                        }))
+
+                    n.enter()
+                        .append("g")
+                        .attr("class", "node")
+                        .append("text")
+                        .attr("dx", function (d) {
+                            return d.x < 180 ? 8 : -8;
+                        })
+                        .attr("dy", ".31em")
+                        .attr("text-anchor", function (d) {
+                            return d.x < 180 ? "start" : "end";
+                        })
+                        .attr("transform", function (d) {
+                            return d.x < 180 ? null : "rotate(180)";
+                        })
+                        .text(function (d) {
+                            return d.key;
+                        });
+
+                    n.exit().remove();
+
+                    n.transition().attr("transform", function (d) {
+                        return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+                    })
+
+                }
+        };
+
+        function clone(obj) {
+            // Handle the 3 simple types, and null or undefined
+            if (null == obj || "object" != typeof obj) return obj;
+
+            // Handle Date
+            if (obj instanceof Date) {
+                var copy = new Date();
+                copy.setTime(obj.getTime());
+                return copy;
+            }
+
+            // Handle Array
+            if (obj instanceof Array) {
+                var copy = [];
+                for (var i = 0, len = obj.length; i < len; i++) {
+                    copy[i] = clone(obj[i]);
+                }
+                return copy;
+            }
+
+            // Handle Object
+            if (obj instanceof Object) {
+                var copy = {};
+                for (var attr in obj) {
+                    if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+                }
+                return copy;
+            }
+
+            throw new Error("Unable to copy obj! Its type isn't supported.");
+        }
+
+        // Lazily construct the package hierarchy from class names.
 		function packageHierarchy(classes) {
 		  var map = {};
 
@@ -142,9 +179,11 @@ myApp.directive('cluster', function(){
 		    return node;
 		  }
 
-		  classes.forEach(function(d) {
-		    find(d.name, d);
-		  });
+		  if(classes) {
+              classes.forEach(function(d) {
+                  find(d.text, d);
+              });
+          }
 
 		  return map[""];
 		}
@@ -154,13 +193,13 @@ myApp.directive('cluster', function(){
 		  var map = {};
 
 		  nodes.forEach( function(d) {
-		    map[d.id] = d;
+		    map[d._id] = d;
 		  });
 
 		  links.forEach( function(d) {
 		  	var e = {};
-			e.source = map[d.source];
-			e.target = map[d.target]
+			e.source = map[d.n_id_orig];
+			e.target = map[d.n_id_dest]
 			imports.push(e);
 		  });
 
@@ -197,15 +236,16 @@ myApp.directive('cluster', function(){
 });
 
 function mainController($scope, $http) {
-	$scope.formNode = {};
-	$scope.formED = {};
-	$scope.newconnobj = {};
-	$scope.edges = {};
+	$scope.info = {}
+    $scope.info.formNode = {};
+	$scope.info.formED = {};
+	$scope.info.newconnobj = {};
+	$scope.info.edges = {};
 
 	// when landing on the page, get all nodes and show them
 	$http.get('/api/nodes')
 		.success(function(data) {
-			$scope.nodes = data;
+			$scope.info.nodes = data;
 			console.log(data);
 		})
 		.error(function(data) {
@@ -215,7 +255,7 @@ function mainController($scope, $http) {
 	// when landing on the page, get all ed descriptions and show them
 	$http.get('/api/edgedesc')
 		.success(function(data) {
-			$scope.edgedesc = data;
+			$scope.info.edgedesc = data;
 		})
 		.error(function(data) {
 			console.log('Error: ' + data);
@@ -223,7 +263,7 @@ function mainController($scope, $http) {
 
 	$http.get('/api/edges')
 		.success(function(data) {
-			$scope.edges = $scope.enrichEdges(data);
+			$scope.info.edges = $scope.enrichEdges(data);
 			console.log($scope.edges);
 		})
 		.error(function(data) {
@@ -235,7 +275,7 @@ function mainController($scope, $http) {
 		$http.post('/api/nodes', $scope.formNode)
 			.success(function(data) {
 				$('input').val('');
-				$scope.nodes = data;
+				$scope.info.nodes = data;
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
@@ -246,7 +286,7 @@ function mainController($scope, $http) {
 	$scope.deleteNode = function(id) {
 		$http.delete('/api/nodes/' + id)
 			.success(function(data) {
-				$scope.nodes = data;
+				$scope.info.nodes = data;
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
@@ -258,7 +298,7 @@ function mainController($scope, $http) {
 		$http.post('/api/edgedesc', $scope.formED)
 			.success(function(data) {
 				$('input').val('');
-				$scope.edgedesc = data;
+				$scope.info.edgedesc = data;
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
@@ -274,10 +314,10 @@ function mainController($scope, $http) {
 	
 	// when submitting the add form, send the text to the node API
 	$scope.createEdge = function() {
-		$http.post('/api/edges', $scope.newconnobj)
+		$http.post('/api/edges', $scope.info.newconnobj)
 			.success(function(data) {
 				$('input').val('');
-				$scope.edges = data;
+				$scope.info.edges = data;
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
@@ -288,7 +328,7 @@ function mainController($scope, $http) {
 	$scope.deleteEdge = function(id) {
 		$http.delete('/api/edges/' + id)
 			.success(function(data) {
-				$scope.edges = $scope.enrichEdges(data);
+				$scope.info.edges = $scope.enrichEdges(data);
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
@@ -299,11 +339,16 @@ function mainController($scope, $http) {
 	$scope.deleteED = function(id) {
 		$http.delete('/api/edgedesc/' + id)
 			.success(function(data) {
-				$scope.edgedesc = data;
+				$scope.info.edgedesc = data;
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
 			});
 	};
+
+
+    $scope.initd3 = function() {
+
+    }
 
 }
